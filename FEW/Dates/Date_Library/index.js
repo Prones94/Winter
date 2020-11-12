ef9e1510371c515db4c2362f94da0b7dfd3b24ad
Ian Rones <ronesp.85@gmail.com>
2020-11-11 20:56:02 -0800

    [add] characters for singles

---
+++ FEW/Dates/Date_Library/index.js
@@ -0,0 +1,52 @@
+const full_months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+// Helper function to check if hour/second/minute is either over or under 10
+const check = num => {
+    num < 10 ? `0${num}` : `${num}`
+}
+
+class Date {
+    constructor(...args) {
+        this.date = new Date(...args)
+        this.dVars = {
+            'Y': this.year,
+            'y': this.year % 100,
+            'M': full_months[this.month],
+            'm': months[this.months],
+            'D': check(this.day),
+            'd': this.day,
+            'H': check(this.hour),
+            'h': this.hour,
+            'F': check(this.minute),
+            'f': this.minute,
+            'S': check(this.second),
+            's': this.second
+        }
+    }
+
+    /*
+        ***************************** NOTE! ********************************
+        Using the JS Getter get syntax to bind object property to a function    that will be called when that property is looked up
+
+        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get#:~:text=The%20get%20syntax%20binds%20an,that%20property%20is%20looked%20up.
+    */
+
+    // Year
+    get year() { return this.date.getFullYear() }
+    // Month
+    get month() { return this.date.getMonth() }
+    // Day
+    get day() { return this.date.getDate() }
+    // Hour
+    get hour() { return this.date.getHours() }
+    // Minute
+    get minute() { return this.date.getMinutes() }
+    //Second
+    get second() { return this.date.getSeconds() }
+
+    
+
+
+}
